63dfc83e7235bd0fa042e451542480871e3af8a3
itmao <1280337559@qq.com>
2022-11-11 16:57:29 +0800

    dd

--- src/lib/ajax.js
+++ src/lib/ajax.js
@@ -4,15 +4,14 @@ import NProgress from 'nprogress'
  *  @BSER_URL 网络请求路径
  * **/
 // const BASE_URL = process.env.NODE_ENV === 'development' ? '/api' : 'https://lianghj.top:3000'
-// const BASE_URL = process.env.NODE_ENV === 'development' ? '/api' : 'https://www.mylapi.icu'
+const BASE_URL = process.env.NODE_ENV === 'development' ? '/api' : 'https://www.mylapi.icu'
 export function ajax (config) {
   // axios实例
   const instance = axios.create({
     // baseURL: 'http://120.78.137.246:3000',
     // baseURL: 'https://lianghj.top:3000',
     // baseURL: 'https://www.mylapi.icu',
-    // baseURL: BASE_URL,
-    baseURL: '/api',
+    baseURL: BASE_URL,
     timeout: 10000
   })
   // 请求拦截
